ff54497edf9d5dd9bdeaaffb8cd53f978e32c8b2
Carlos Eduardo Santos Freitas <carlos.freitas@valmetpartners.com>
2024-04-29 13:37:08 -0300

    Creating second screen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme'
 import { Loading } from '@components/Loading';
@@ -21,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }
