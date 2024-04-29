b58d506b1a7e84c4f6e3ae9d8c56a785d6c01a33
Carlos Freitas <carlos.edfrei@gmail.com>
2024-04-29 08:18:21 -0300

    Loading screen and Header

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme'
-import { ActivityIndicator } from 'react-native'
-import { 
-        useFonts, 
-        Roboto_400Regular, 
-        Roboto_700Bold } 
+import { Loading } from '@components/Loading';
+import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+}
   from '@expo-google-fonts/roboto'
+import React from 'react';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ?  <Groups /> : <ActivityIndicator/>}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
