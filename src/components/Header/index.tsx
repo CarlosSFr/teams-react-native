1f762cc2b5ce3361cedb3db61f54fe411c3f546b
Carlos Eduardo Santos Freitas <carlos.freitas@valmetpartners.com>
2024-04-29 10:34:25 -0300

    New components added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { Container, Logo } from "./styles";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 import logoImg from "@assets/logo.png"
+import { CaretLeft } from "phosphor-react-native";
 
-export function Header() {
+type Props = {
+    showBackButton?: boolean;
+}
+
+export function Header({showBackButton = false}: Props) {
     return (
         <Container>
+            { showBackButton &&
+            <BackButton>
+                <BackIcon />
+            </BackButton>
+            }
             <Logo source={logoImg} />
         </Container>
     )
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Container } from './styles';
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight 
+        title="Turmas"
+        subtitle='Jogue com a galera'
+      />
+      <GroupCard 
+        title="Galera da UTFPR"
+      />
     </Container>
   );
 }
